357d4f37b44b6707a819d9f1ccfecfb085f6cd29
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-05 23:39:09 +0300

    login_view

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate,login
 # Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("/")
+        else:
+            return render(request,"login.html",{"error":"Invalid username or password"})
+
+    return render(request,"login.html") 
--- crudcrm/settings.py
+++ crudcrm/settings.py
@@ -92,13 +92,6 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 
-AUTHENTICATION_BACKENDS = [
-
-
-    # `allauth` specific authentication methods, such as login by email
-    'allauth.account.auth_backends.AuthenticationBackend',
-    ...
-]
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -151,3 +144,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+]
