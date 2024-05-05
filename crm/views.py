8ef6b4a0934e2a8eeec87aa8582be98cb2157588
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-05 23:43:36 +0300

    register_view

--- crm/views.py
+++ crm/views.py
@@ -23,3 +23,21 @@ def login_view(request):
             return render(request,"login.html",{"error":"Invalid username or password"})
 
     return render(request,"login.html") 
+
+def register_view(request):
+    if request.method == "POST":
+        first_name =first_name
+        last_name = last_name
+        username = username
+        email = email
+        password = password
+        confirm_password = confirm_password
+
+        if password == confirm_password:
+            user = User.objects.create_user(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+            user.save()
+            return redirect("/")
+        else:
+            return render(request,"register.html",{"error":"Passwords do not match"})
+
+    return render(request,"register.html")
